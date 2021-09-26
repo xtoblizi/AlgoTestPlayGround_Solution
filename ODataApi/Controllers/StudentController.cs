f9fcce4e230397bb81657b2fb8d3d4a463057564
Ogbosuka Christopher <ogbosukachris@gmail.com>
2021-09-27 00:56:29 +0100

    update project test with OData

---
+++ ODataApi/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataApi.Controllers
+{
+	public class StudentController: ControllerBase
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		public StudentController(IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+		}
+
+		public async Task<IActionResult> GetStudent()
+		{
+			return Ok(await Task.FromResult(GetStudents));
+
+		}
+
+		private List<Student> GetStudents => new List<Student>
+		{
+			new Student
+			{
+				Id = 1,
+				Name = "Chris",
+				Score = 1324,
+			},
+			new Student
+			{
+				Id = 2,
+				Name = "Daniel",
+				Score = 43453.049m
+			},
+			new Student
+			{
+				Id = 3,
+				Name = "Dammy",
+				Score = 903403
+			}
+		};
+	}
+}
